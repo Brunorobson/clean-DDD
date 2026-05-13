858575482adaaf0687b72abfabdaadb6aa4d90b1
Bruno Robson <brobsonsites@gmail.com>
2026-05-13 14:19:08 -0300

    casos de uso e entidades

---
+++ src/domain/use-cases/answer-question.spec.ts
@@ -0,0 +1,19 @@
+import { expect, test } from 'vitest'
+import { AnswerQuestionUseCase } from './answer-question'
+import { AnswersRepository } from '../repositories/answers-repository'
+const fakeAnswersRepository: AnswersRepository = {
+    create: async (answer) => {
+        return
+    }
+}
+
+test('create an answer', async () => {
+    const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
+    const answer = await answerQuestion.execute({
+        questionId: 'question-1',
+        instructorId: 'instructor-1',
+        content: 'This is the answer'
+    })
+
+    expect(answer.content).toEqual('This is the answer')
+})
