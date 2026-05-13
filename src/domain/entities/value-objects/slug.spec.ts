eb68a1baed1199dcfd624f21efb3c0cf96309138
Bruno Robson <brobsonsites@gmail.com>
2026-05-13 16:45:24 -0300

    Value Object de slug

---
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -0,0 +1,7 @@
+import { expect, test } from 'vitest'
+import { Slug } from './slug'
+
+test('create a slug from a string', () => {
+    const slug = Slug.createFromText("How to create a slug")
+    expect(slug.value).toEqual("how-to-create-a-slug")
+})
---
+++ src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,26 @@
+export class Slug{
+    public value: string
+    constructor(value: string){
+        this.value = value
+    }
+
+    /**
+     * Recives a string and returns a slugified version of it a slug.
+     * Example: "How to create a slug?" => "how-to-create-a-slug"
+     * @param text {string}
+     */
+
+    static createFromText(text: string){
+        const slugifiedText = text
+        .normalize("NFKD")
+        .toLowerCase()
+        .trim()
+        .replace(/\s+/g, "-")
+        .replace(/[^\w\-]+/g, "")
+        .replace(/\-\-+/g, "-")
+        .replace(/--/g, "-")
+        .replace(/-$/g, "")
+        return new Slug(slugifiedText)
+    }
+
+}
