cb23d15b745fe35494f4e4be81cd20af841423be
Brunorobson <brobsonsites@gmail.com>
2026-06-03 18:59:25 -0300

    Getters & Setters das entidades

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -14,15 +14,40 @@ export class Answer extends Entity<AnswerProps>{
     get content(){
         return this.props.content
     }
+        get authorId(){
+        return this.props.authorId
+    }
+        get questionId(){
+        return this.props.questionId
+    }
+        get createdAt(){
+        return this.props.createdAt
+    }
+        get updateAt(){
+        return this.props.updateAt
+    }
+
+    get excerpt(){
+        return  this.content.substring(0, 120).trimEnd().concat('...')
+    }
+
+    private touch(){
+        this.props.updateAt = new Date()
+    }
+
+    set content(content:string){
+        this.props.content = content
+        this.touch()
+    }
 
     static create(
-              props: Optional<AnswerProps, 'createdAt'>, 
-              id?: UniqueEntityID
-         ){
-              const answer = new Answer({
-              ...props,
-              createdAt: new Date(),
-              }, id)
-              return answer
-         }
+        props: Optional<AnswerProps, 'createdAt'>, 
+        id?: UniqueEntityID
+        ){
+        const answer = new Answer({
+        ...props,
+        createdAt: new Date(),
+        }, id)
+        return answer
+        }
 }
