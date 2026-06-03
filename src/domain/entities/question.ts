84a99e79f7452d22adef6932b98855238c85529f
Brunorobson <brobsonsites@gmail.com>
2026-06-03 17:51:29 -0300

    Mapeando propriedades

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,15 @@
 import { randomUUID } from "node:crypto"
 import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 interface QuestionProps {
      title: string,
+     bestAnswerId?: UniqueEntityID
      content: string,
      slug: Slug,
-     authorId: string
+     authorId: UniqueEntityID
+    createdAt: Date
+    updateAt?: Date
 }
 export class Question extends Entity<QuestionProps> {
 }
