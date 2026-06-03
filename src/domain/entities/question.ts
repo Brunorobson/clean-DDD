5b3b30f4688b8cb7233f5967f670f9b03375ebbe
Brunorobson <brobsonsites@gmail.com>
2026-06-03 18:17:03 -0300

    Abstraindo criação de entidades

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -2,14 +2,25 @@ import { randomUUID } from "node:crypto"
 import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
 import { UniqueEntityID } from "../../core/entities/unique-entity-id"
+import { Optional } from "../../core/types/optional"
 interface QuestionProps {
      title: string,
      bestAnswerId?: UniqueEntityID
      content: string,
      slug: Slug,
      authorId: UniqueEntityID
-    createdAt: Date
-    updateAt?: Date
+     createdAt: Date
+     updateAt?: Date
 }
 export class Question extends Entity<QuestionProps> {
+     static create(
+          props: Optional<QuestionProps, 'createdAt'>, 
+          id?: UniqueEntityID
+     ){
+          const question = new Question({
+          ...props,
+          createdAt: new Date(),
+          }, id)
+          return question
+     }
 }
